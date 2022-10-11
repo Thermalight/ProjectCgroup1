ad4a7f9126ba78aedecfeea77d1149e5c84d361a
Thermalight <alumite10@gmail.com>
2022-10-12 01:16:23 +0200

    Add default entries to the database

--- Api/Services/DatabaseService.cs
+++ Api/Services/DatabaseService.cs
@@ -1,5 +1,6 @@
 using ChengetaWebApp.Api.Database;
 using ChengetaWebApp.Api.Database.Models;
+using Bcrypt = BCrypt.Net.BCrypt;
 
 namespace ChengetaWebApp.Api.Services;
 
@@ -14,6 +15,24 @@ public class DatabaseService
 
         using var dbContext = new SqliteDbContext();
         await dbContext.Database.EnsureCreatedAsync();
+        await dbContext.SaveChangesAsync();
+
+        // create all priorities
+        await dbContext.AddAsync(new SoundPriority(){SoundType="gunshot", Priority=1});
+        await dbContext.AddAsync(new SoundPriority(){SoundType="vehicle", Priority=2});
+        await dbContext.AddAsync(new SoundPriority(){SoundType="animal", Priority=3});
+        await dbContext.AddAsync(new SoundPriority(){SoundType="unknown", Priority=4});
+        await dbContext.SaveChangesAsync();
+
+        // create all statustypes
+        await dbContext.AddAsync(new Status(){Description="A ranger is checking out the event", Name="Being handled"});
+        await dbContext.AddAsync(new Status(){Description="The notification is not being handled yet", Name="Not handled"});
+        await dbContext.AddAsync(new Status(){Description="The event has been closed", Name="Handled"});
+        await dbContext.AddAsync(new Status(){Description="The notification was not what it appeared to be", Name="False alarm"});
+        await dbContext.SaveChangesAsync();
+
+        // create admin account
+        await dbContext.AddAsync(new User(){Username="Admin", Password=Bcrypt.HashPassword("Password123!"), IsAdmin=true, Email="debug@debug.com"});
         await dbContext.SaveChangesAsync();
     }
 
