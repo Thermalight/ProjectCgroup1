8750c4ca6e1b77b7585fc270476aee043da68f18
Thermalight <alumite10@gmail.com>
2022-10-12 01:15:38 +0200

    Define SoundPriority model

---
+++ Api/Database/Models/SoundPriority.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ChengetaWebApp.Api.Database.Models;
+
+public class SoundPriority
+{
+    [Key]
+    public string SoundType { get; set; }
+    public int Priority { get; set; } 
+    public List<Notification> Notifications { get; set; }
+}
