507bd11dadeb1fc0dd88ad957f6e508956af4350
Thermalight <alumite10@gmail.com>
2022-10-12 01:13:55 +0200

    Define models

--- Api/Database/SqliteDbContext.cs
+++ Api/Database/SqliteDbContext.cs
@@ -6,7 +6,29 @@ namespace ChengetaWebApp.Api.Database
     public class SqliteDbContext : DbContext
     {
         public DbSet<Notification> Notifications { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Subscriber> Subscribers { get; set; }
+        public DbSet<SoundPriority> Priorities { get; set; }
+        public DbSet<Status> Statuses { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Notification>()
+                .HasOne(n => n.Priority)
+                .WithMany(p => p.Notifications)
+                .HasForeignKey(n => n.SoundType);
+
+            modelBuilder.Entity<Notification>()
+                .HasOne(n => n.Status)
+                .WithMany(p => p.Notifications)
+                .HasForeignKey(n => n.StatusID);
+
+            modelBuilder.Entity<Subscriber>()
+                .HasOne(s => s.User)
+                .WithOne(u => u.Subscriber)
+                .HasForeignKey<Subscriber>(s => s.UserID);
+        }
+        
         public static SqliteDbContext Create()
             => new();
 
