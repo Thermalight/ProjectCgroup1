d3151b2e2b9a76672367c59519214766d745092d
MustacheAltun <51758662+MustacheAltun@users.noreply.github.com>
2022-10-12 14:02:39 +0200

    login code van login branch
    omdat het rebasen niet werkt heb ik een nieuwe branch gemaakt met de originele functies.

--- svelte-app/App.svelte
+++ svelte-app/App.svelte
@@ -1,10 +1,12 @@
 <script>
+    import Login from "./login.svelte";
     export let name;
 </script>
 <style global lang="postcss">
 
 </style>
 <main>
-    <h1>Hello {name}!</h1>
+    <!-- <h1>Hello {name}!</h1> -->
+    <Login/>
 </main>
 
---
+++ svelte-app/login.svelte
@@ -0,0 +1,57 @@
+<script>
+    let seePassword = false;
+    $: type = seePassword ? 'text' : 'password'
+    let value = '';
+
+    function onInput (event) {
+	    value = event.target.value
+    }
+</script>
+<style>
+    a:active {
+    color: gray;
+    }
+    a {
+    color: white;
+    }
+    .customButton:active{
+        background-color: transparent;
+    }
+</style>
+<div class="h-full">
+    <div style="background-color:#363e4c;"class="block  h-4/6 w-5/6 m-auto rounded-2xl">
+        <div style="background-color:#111727;"class="flex justify-center   h-16 rounded-t-2xl p-auto">
+            <h1 class="text-2xl text-white m-auto">Log in</h1>
+        </div>
+        <div style="height: 89%;"class="grid w-4/5 content-center m-auto py-16">
+            <form>
+                <div class="flex relative z-0 mb-6 w-full group">
+                    <span class="material-symbols-outlined m-auto text-white">mail</span>
+                    <input type="email" name="floating_email" id="floating_email" class="block  w-full text-sm text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer h-9 ml-3" placeholder="Email address" required>
+                </div>
+                
+                <div class="flex relative z-0 mb-6 w-full group">
+                    <span class="material-symbols-outlined m-auto text-white">lock</span>
+                    <input { type } name="floating_password" on:input={ onInput } id="floating_password" class=" text-white ml-3 mr-1 block  w-full text-sm  bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Password" required>
+                    <button type="button" class="btn border-none customButton" on:click="{ () => seePassword = !seePassword }">
+                        {#if seePassword}
+                            <!-- see -->
+                            <span class="material-symbols-outlined m-auto text-white">visibility</span>
+                        {:else}
+                            <!-- unsee -->
+                            <span class="material-symbols-outlined m-auto text-white">visibility_off</span>
+                        {/if}
+                    </button>
+                </div>
+                <div align="right ">
+
+                    <a href="#!"  class="right-0 underline text-xs duration-200 transition ease-in-out ">Forgot password?</a>
+                
+                </div>
+                <div class="flex align-items justify-content mt-10">
+                    <button style="background-color:#111727"type="submit" class="  text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 border-none m-auto p-auto">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
