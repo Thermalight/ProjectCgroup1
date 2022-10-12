7513a6ae3f56077ac2cba3ac413dad7bda81f089
Thermalight <alumite10@gmail.com>
2022-10-12 17:12:51 +0200

    Remove template file

--- svelte-app/Stepper.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    export let count = 0;
-  </script>
-  
-  <button aria-label="decrement" on:click={() => count--}>-</button>
-  <span data-cy="count">{count}</span>
-  <button aria-label="increment" on:click={() => count++}>+</button>
