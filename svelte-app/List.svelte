3a405fc884e641a8e8366fe9351d373b445d90ef
Thermalight <alumite10@gmail.com>
2022-10-12 20:09:27 +0200

    Add notification components to a list component

---
+++ svelte-app/List.svelte
@@ -0,0 +1,27 @@
+<script>
+    import Notification from "./Notification.svelte"
+    
+    var notifications;
+    fetch("https://localhost/notifications", {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => notifications = data)
+        .then(() => console.log(notifications));
+</script>
+<div class="list">
+    {#if notifications != null}
+        {#each notifications as event}
+            <Notification event={event}/>
+        {/each}
+    {/if}
+</div>
+<style>
+    .list {
+        background-color: #363E4C;
+        padding: 10px;
+    }
+</style>
