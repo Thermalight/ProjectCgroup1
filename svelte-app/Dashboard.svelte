52ee15b652649bf805fe8886ce548ec058252f39
E-Bas <100128509+E-Bas@users.noreply.github.com>
2022-10-12 11:16:39 +0200

    Create dashboard

---
+++ svelte-app/Dashboard.svelte
@@ -0,0 +1,67 @@
+<div class="sidebar"> 
+    <nav>
+        <!-- here we'll put in the links to the other pages in the hamburger menu, as well as any code styling the hamburger menu -->
+        <ul>
+            <li>
+                <!-- every list item wil be a link so <a></a> so to the dashboard, to the list of notivs, to user management or to settings -->
+                <a href="#">Dashboard</a>
+            </li>
+            <li>
+                <!-- every list item wil be a link so <a></a> so to the dashboard, to the list of notivs, to user management or to settings -->
+                <a href="#">Notifications</a>
+            </li>
+            <li>
+                <!-- every list item wil be a link so <a></a> so to the dashboard, to the list of notivs, to user management or to settings -->
+                <a href="#">User Management</a>
+            </li>
+            <li>
+                <!-- every list item wil be a link so <a></a> so to the dashboard, to the list of notivs, to user management or to settings -->
+                <a href="#">Settings</a>
+            </li>
+        </ul>
+    </nav>
+</div>
+
+<nav class="navigation-bar">
+    <div class="hamburger-menu">
+        <span class="material-symbols-outlined"> menu </span>
+    </div>
+
+    <div class="logo">
+        <img src="images/icon.png" alt="Chengetta logo consisting of an elephant with the words Chengetta and wildlife underneath it"/>
+    </div>
+</nav>
+
+<div class="dashboard-content">
+    <div class="map-header">
+        <div class="sorted-button">
+            <!-- here we put the sorted button -->
+            <button type="button" class="rounded-full ..."><span class="material-symbols-outlined"> sort </span></button>
+        </div>
+
+        <h1> map </h1>
+
+        <div class="filter-button">
+            <!-- here we put the filter button -->
+            <button type="button" class="rounded-full ..."><span class="material-symbols-outlined"> filter_alt </span></button>
+        </div>
+    </div>
+
+    <div class="placeholder-map">
+        <p> this is a placeholder map </p>
+    </div>
+
+    <div class="placeholder-list-of-notifications">
+        <div class="scroll-bar"> 
+            <p> scrolly scrolly </p>
+        </div>
+
+        <div class="placeholder-notification-1">
+            <p> this is placeholder notification 1 </p>
+        </div>
+    </div>
+</div>
+
+<div class="footer">
+    <p> this is a footer </p>
+</div>
