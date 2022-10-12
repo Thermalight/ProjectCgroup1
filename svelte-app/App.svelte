eb24891617b7bb20272d1d9d6df3e7f8c14921cd
MustacheAltun <51758662+MustacheAltun@users.noreply.github.com>
2022-10-12 14:14:17 +0200

    added banner svelte

--- svelte-app/App.svelte
+++ svelte-app/App.svelte
@@ -1,12 +1,13 @@
 <script>
     import Login from "./login.svelte";
+    import Banner from "./Banner.svelte";
     export let name;
 </script>
 <style global lang="postcss">
 
 </style>
 <main>
-    <!-- <h1>Hello {name}!</h1> -->
+    <Banner/>
     <Login/>
 </main>
 
