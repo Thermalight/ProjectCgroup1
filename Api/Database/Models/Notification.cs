242e2c3638c4c9789a9a8e0938481df0f0bb9107
Thermalight <alumite10@gmail.com>
2022-10-16 18:38:19 +0200

    make fields nullable

--- Api/Database/Models/Notification.cs
+++ Api/Database/Models/Notification.cs
@@ -12,10 +12,10 @@ public class Notification
     public double Latitude { get; set; }
     public double Longitude { get; set; }
     [JsonPropertyName("sound_type")]
-    public string SoundType { get; set; }
+    public string? SoundType { get; set; }
     public SoundPriority? Priority { get; set; }
     public int Probability { get; set; }
-    public string Sound { get; set; }
+    public string? Sound { get; set; }
     public int StatusID  { get; set; } = 1;
     public Status? Status { get; set; }
 }
--- Api/Database/Models/User.cs
+++ Api/Database/Models/User.cs
@@ -6,9 +6,9 @@ public class User
 {
     [Key]
     public Guid GUID { get; set; } = Guid.NewGuid();
-    public string Username { get; set; }
-    public string Password { get; set; }
-    public string Email { get; set; }
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    public string? Email { get; set; }
     public bool IsAdmin { get; set; } = false;
     public Subscriber? Subscriber { get; set; } = null;
 }
