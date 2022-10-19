28e8cc1b7ebedece754d1ff74edbec255bf8b7dc
Leon Bleuanus <bleujr@hotmail.nl>
2022-10-19 10:12:15 +0200

    Opzet gemaakt voor de settingspage

---
+++ svelte-app/Setting.svelte
@@ -0,0 +1,13 @@
+<main>    
+    <h1>Settings</h1>
+    <p>At this page you can change the settings.</p>
+    <h1>Notifications</h1>
+    <input type="checkbox" id="e-mail" name="e-mail">
+    <label for="e-mail"> E-mail notification</label>
+    <input type="checkbox" id="push" name="push">
+    <label for="push"> Push notification</label>
+    <h1>E-mail</h1>
+    <label for="email">E-mailadres for notification:</label>
+    <input type="email" id="email" name="email"><br><br>
+    <input type="submit" value="Submit">
+</main>
