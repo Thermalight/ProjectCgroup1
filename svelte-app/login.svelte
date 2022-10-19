2006c445bd858ada8b57ec52e567a93cb82fd12a
MustacheAltun <51758662+MustacheAltun@users.noreply.github.com>
2022-10-19 20:12:30 +0200

    login error functie is gemaakt

--- svelte-app/login.svelte
+++ svelte-app/login.svelte
@@ -1,36 +1,53 @@
 <script>
+  import { dataset_dev, element } from "svelte/internal";
+
+
     let seePassword = false;
     $: type = seePassword ? 'text' : 'password'
+    let seeError = "hidden";
     let value = '';
-    // var myCookie = getCookie("LoggedIn");
     function onInput (event) {
 	    value = event.target.value
     }
 
-
-    function getCookie(c_name) {
-        if (document.cookie.length > 0) {
-            c_start = document.cookie.indexOf(c_name + "=");
-            if (c_start != -1) {
-                c_start = c_start + c_name.length + 1;
-                c_end = document.cookie.indexOf(";", c_start);
-                if (c_end == -1) {
-                    c_end = document.cookie.length;
-                }
-                return document.cookie.substring(c_start, c_end);
+    //get cookie value
+    function getCookie(name) {
+        const cDecoded = decodeURIComponent(document.cookie);
+        const cArray = cDecoded.split("; ")
+        let result = null;
+        cArray.forEach(Element =>{
+            if (Element.indexOf(name) == 0) {
+                result = Element.substring(name.length+1);
             }
-        }
-        return "";
+        })
+        console.log(result);
+        return result;
     }
-    // alert(getCookie("LoggedIn"));
-    // if (document.cookie.indexOf('LoggedIn') == -1) {
-    //     // do cookie doesn't exist stuff;
-    //     alert("cookie does not exist");
-    // }
-    // else {
-    //     // do cookie exists stuff
-    //     alert("cookie exists");
-    // }
+    //make cookie
+    function setCookie(name,value,daysToLive) {
+        let date = new Date();
+        date.setTime(date.getTime() + (daysToLive * 24 * 60 * 60 * 1000));
+        let expires = "expires=" + date.toUTCString();
+        document.cookie = `${name} = ${value}; ${expires}; path=/`
+    }
+
+    //delete cookie
+    function deleteCookie(name) {
+        setCookie(name,null,null);
+    }
+
+    if (document.cookie.indexOf('LoggedIn') > -1) {
+        // do cookie exists stuff
+        getCookie("LoggedIn")
+        if (getCookie("LoggedIn") == "False") {
+            seeError = "block";
+            deleteCookie("LoggedIn");
+        }
+    }
+    else {
+        // do cookie doesn't exist stuff;
+        seeError = "hidden";
+    }
 </script>
 <style>
     a:active {
@@ -49,6 +66,11 @@
             <h1 class="text-2xl text-white m-auto">Log in</h1>
         </div>
         <div style="height: 89%;"class="grid w-4/5 content-center m-auto py-16">
+            <div role="alert" class="{seeError}">
+                <div class="border border-t-0 border-red-400 rounded-lg bg-red-100  text-red-700 text-center">
+                  <p>Wrong credentials.</p>
+                </div>
+            </div>
             <form autocomplete="off" method="POST" action="/login" name="login">
                 <div class="flex relative z-0 mb-6 w-full group">
                     <span class="material-symbols-outlined m-auto text-white">mail</span>
