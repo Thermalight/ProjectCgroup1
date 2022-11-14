d959eace75532a717a9bd06f7748d19b8bb8b763
Gerrit van Aalst <1035187@hr.nl>
2022-11-14 19:21:10 +0100

    add the abbility to see users in the admin page

---
+++ svelte-app/user-management.svelte
@@ -0,0 +1,33 @@
+
+ <script>
+    import User from './User.svelte';
+    import SearchUser from './SearchUser.svelte';;
+ 
+
+ 
+    var Users;
+    fetch("https://localhost/users", {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then(response => response.json())
+        .then(data => Users = data)
+        .finally
+</script>
+<div class="listOfUsers">
+ {#if Users != null}
+     {#each Users as event}
+         <div>
+             <User event={event}/>
+         </div>
+     {/each}
+ {:else}
+     <div>No users available</div>
+ {/if}
+</div>
+
+
+<SearchUser/>
+
