bcf93af578525234ec188b6874f8c79235f61980
Gerrit van Aalst <1035187@hr.nl>
2022-11-14 19:21:10 +0100

    add the abbility to see users in the admin page

---
+++ svelte-app/SearchUser.svelte
@@ -0,0 +1,20 @@
+<script>
+    import User from './User.svelte';
+    let users2 = ['Esmee','Gerrit', 'Leon', 'Admin', 'Test123']
+    let searchTerm = "";
+    //export let listofUser;
+</script>
+
+<input bind:value={searchTerm} type="Text">
+{#each users2 as user}
+    {#if user.toLowerCase().includes(searchTerm.toLowerCase())}
+        <User event={user}/>
+    {/if}
+{/each}
+
+
+
+
+
+
+
---
+++ svelte-app/User.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let event;
+</script>
+{#if event != null}
+    <div>
+        <div class="content">
+            <p>username: {event.Username}</p>
+            <p>email: {event.Email}</p>
+            {#if event.IsAdmin == false || event.IsAdmin == null}
+                <p>is admin: no</p>
+            {:else}
+                <p>is admin: yes</p>
+            {/if}
+            {#if event.Subscriber == null}
+                <p>is subscribed: no</p>
+            {:else}
+                <p>is subscribed: yes</p>
+            {/if}
+        </div>
+    </div>
+{/if}
+
