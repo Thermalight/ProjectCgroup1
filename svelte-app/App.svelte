19a7f43022991151ec7944e9d339fe708dd2dd99
Gerrit van Aalst <1035187@hr.nl>
2022-11-14 19:21:10 +0100

    add the abbility to see users in the admin page

--- svelte-app/App.svelte
+++ svelte-app/App.svelte
@@ -1,10 +1,12 @@
 <script>
     export let name;
+    import UserManagement from "./user-management.svelte";
 </script>
 <style global lang="postcss">
 
 </style>
 <main>
-    <h1>Hello {name}!</h1>
+    <!-- <h1>Hello {name}!</h1> -->
+    <UserManagement/> 
 </main>
 
