8c9e6a9977d9ff66498c241fa216fbb04855958e
MustacheAltun <51758662+MustacheAltun@users.noreply.github.com>
2022-11-15 20:40:53 +0100

    login branche clean up

--- svelte-app/App.svelte
+++ svelte-app/App.svelte
@@ -1,13 +1,9 @@
 <script>
-    import Login from "./login.svelte";
-    import Banner from "./Banner.svelte";
     export let name;
 </script>
 <style global lang="postcss">
 
 </style>
 <main>
-    <Banner/>
-    <Login/>
 </main>
 
