5423333ae28abb73646187fb3f6c143f2dd7fb9c
Thermalight <alumite10@gmail.com>
2022-11-15 20:16:00 +0100

    declutter user component

--- svelte-app/User.svelte
+++ svelte-app/User.svelte
@@ -1,25 +1,13 @@
 <script>
-    export let event;
+    export let user;
 </script>
-{#if event != null}
+{#if user}
     <div>
-        <div class="content">
-            <p class="text-white">username: {event.Username}</p>
-            <p class="text-white">email: {event.Email}</p>
-            {#if event.IsAdmin == false || event.IsAdmin == null}
-                <p class="text-white">is admin: no</p>
-            {:else}
-            <p class="text-white">is admin: yes</p>
-            {/if}
-            {#if event.Subscriber == null}
-            <p class="text-white">is subscribed: no</p>
-            {:else}
-            <p class="text-white">is subscribed: yes</p>
-            {/if}
+        <div class="content text-white">
+            <p>username: {console.log(user)}</p>
+            <p>email: {user.Email}</p>
+            <p>admin: {user.IsAdmin ? "True" : "False"}</p>
+            <p>subscribed: {user.Subscriber ? "True" : "False"}</p>
         </div>
     </div>
 {/if}
-
-
-
-
