74fc6c72e8a9726559184e43ca11ca0f7372d0f9
E-Bas <100128509+E-Bas@users.noreply.github.com>
2022-11-21 19:21:19 +0100

    add header with navbar

--- svelte-app/App.svelte
+++ svelte-app/App.svelte
@@ -4,6 +4,7 @@
     import { currentPage } from "./stores.js";
     import Login from "./login.svelte";
     import Root from "./root.svelte";
+    import Navbar from './Navbar.svelte'
     let routes = {
         "notificationpage": List,
         "dashboard": Dashboard,
@@ -17,6 +18,9 @@
 <style global lang="postcss">
 
 </style>
+<header>
+    <Navbar/>
+</header>
 <main>
     <svelte:component this={routes[$currentPage.toLowerCase()]} />
 </main>
