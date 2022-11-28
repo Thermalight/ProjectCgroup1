f21452b2885977eaccc522179b8c7f64c2d6a260
Leon Bleuanus <bleujr@hotmail.nl>
2022-11-28 15:59:21 +0100

    Thema toegevoegd

--- svelte-app/Setting.svelte
+++ svelte-app/Setting.svelte
@@ -1,13 +1,17 @@
-<main>    
-    <h1>Settings</h1>
-    <p>At this page you can change the settings.</p>
-    <h1>Notifications</h1>
-    <input type="checkbox" id="e-mail" name="e-mail">
-    <label for="e-mail"> E-mail notification</label>
-    <input type="checkbox" id="push" name="push">
-    <label for="push"> Push notification</label>
-    <h1>E-mail</h1>
-    <label for="email">E-mailadres for notification:</label>
-    <input type="email" id="email" name="email"><br><br>
-    <input type="submit" value="Submit">
-</main>
+<h1>Settings</h1>
+<p>At this page you can change the settings.</p>
+<h1>Notifications</h1>
+<label for="e-mail"> E-mail notification</label>
+<input type="checkbox" id="e-mail" name="e-mail">
+<label for="push"> Push notification</label>
+<input type="checkbox" id="push" name="push">
+<h1>E-mail</h1>
+<label for="email">E-mailadres for notification:</label>
+<input type="email" id="email" name="email">
+<h1>Theme</h1>
+<input list="theme">
+<datalist id="theme">
+    <option value="Dark">
+    <option value="Light">
+</datalist><br><br>
+<input type="submit" value="Submit">
