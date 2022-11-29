5d2b5c63424f15835e966abdfe661a639227a847
Leon Bleuanus <bleujr@hotmail.nl>
2022-11-29 16:07:19 +0100

    Prioriteitenlijst toegevoegd

--- svelte-app/Setting.svelte
+++ svelte-app/Setting.svelte
@@ -4,6 +4,13 @@
 <label for="probability"> Probability</label>
 <input type="range" id="probability" min="0" max="100" value="50" oninput="rangeValue.innerText = this.value">
 <p id="rangeValue">50</p>
+<p>Of which status category do you want notifications? Select your options:</p>
+<label for="status1"> Gunshot</label>
+<input type="checkbox" id="status1" name="status1" value="Gunshot">
+<label for="status2"> Car</label>
+<input type="checkbox" id="status2" name="status2" value="Car">
+<label for="status3"> Unknown</label>
+<input type="checkbox" id="status3" name="status3" value="Unknown">
 <label for="e-mail"> E-mail notification</label>
 <input type="checkbox" id="e-mail" name="e-mail">
 <label for="push"> Push notification</label>
