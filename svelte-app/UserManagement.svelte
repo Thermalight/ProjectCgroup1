cc78a50e6e2b19151386665338a24853cdeabe17
E-Bas <100128509+E-Bas@users.noreply.github.com>
2022-11-29 21:29:04 +0100

    yikes

--- svelte-app/UserManagement.svelte
+++ svelte-app/UserManagement.svelte
@@ -2,9 +2,11 @@
     import User from './User.svelte';
     import Input from './Input.svelte';
     let searchTerm = "";
-    let UserJson = {username : "", password: "", email: "", IsAdmin: true};
+    let test = ""
+    let UserJson = {username : "", password: "", email: "", IsAdmin: false};
 	
 	async function submitHandler() {
+        console.log(UserJson)
 		const response = await fetch("https://localhost/user", {
             method: "POST",
             headers: {
@@ -16,7 +18,7 @@
         return await response.json();
 	}
 
-    export async function loadUsers () { 
+    export async function loadUsers() { 
         const response = await fetch("https://localhost/users", {
             method: "GET",
             headers: {
@@ -25,6 +27,30 @@
         });
         return await response.json();
     }
+
+    // async function deleteUser() {
+	// 	const response = await fetch("https://localhost/user", {
+    //         method: "DELETE",
+    //         headers: {
+    //             "Content-Type": "application/json",
+    //         },
+    //         body: //iets hier,
+    //     });
+    //     location.reload();
+    //     return await response.json();
+	// }
+
+    function isAdmin() {
+        if (test == "true") {
+            UserJson.IsAdmin = true
+        }
+        else {
+           UserJson.IsAdmin = false
+        }
+        test = ""
+        return 
+    }
+
 </script>
 
 <div >
@@ -36,15 +62,17 @@
     <div>
         <!-- input form to add users -->
         <form on:submit={submitHandler} class="mt-6">
-            <Input label="Username" bind:value={UserJson.username} />
+            <Input type="text" label="Username" bind:value={UserJson.username} />
             <Input type="password" label="Password" bind:value={UserJson.password} />
             <Input type="email" label="Email" bind:value={UserJson.email}/>
-            <button class="bg-white px-8 py-2 mt-1 border-none border-r-4 text-gray-800">click</button>
+            <!-- <label class="text-white" for="isadmin">Is Admin: </label> -->
+            <Input type="checkbox" label="isAdmin" bind:value={UserJson.IsAdmin}/>
+            <!-- <input type="checkbox" id="isadmin" name="isadmin" value={UserJson.IsAdmin = true}<br> -->
+            <button class="bg-white px-8 py-2 mt-1 border-none border-r-4 text-gray-800">submit</button>
         </form>
     </div>
 
     <br><hr>
-
     <p class="text-white">Users:</p>
     <!-- display users -->
     {#await loadUsers()}
@@ -53,7 +81,7 @@
         {#each data as user}
             {#if user.Username.toLowerCase().includes(searchTerm.toLowerCase())}
                 <div>
-                    <User user={user} />
+                    <User user={user} /><br>
                 </div>
             {/if}
         {/each}
