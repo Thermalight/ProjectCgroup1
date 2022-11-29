f1e987a9ff638578ef655d907768384dd7fb5e84
MustacheAltun <51758662+MustacheAltun@users.noreply.github.com>
2022-11-29 22:19:51 +0100

    button

--- svelte-app/Notification.svelte
+++ svelte-app/Notification.svelte
@@ -4,6 +4,8 @@
     export let mapComponent
     export let event;
     let open = false
+    let play = true
+    let pause = false
     let time = new Date(event.Time*1000)
     let hours = time.getHours()
     let minutes = time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes()
@@ -15,12 +17,24 @@
     function toggle() {
         open = !open
     }
+    function toggleSound() {
+        play = !play
+        pause = !pause
+    }
 </script>
 
 {#if event != null}
     <div on:click={mapComponent.flyToLocation(event.Latitude, event.Longitude)}>
         <div on:click={toggle} class="{event.sound_type} p-4 text-white bg-primary-dark mb-2 rounded-lg">
             <div class="content">
+                <audio id="player" src="{event.Sound}"></audio>
+                {#if play == true}
+                    <button on:click={toggleSound} class="border-transparent focus:border-transparent focus:ring-0" onclick="document.getElementById('player').play()"><span class="material-symbols-outlined">play_circle</span></button>
+                {:else}
+                    <button on:click={toggleSound} class="border-transparent focus:border-transparent focus:ring-0 " onclick="document.getElementById('player').pause()"><span class="material-symbols-outlined">pause_circle</span></button>
+                {/if}
+                
+
                 <p class="font-bold">{event.sound_type}</p>
                 <Status status={event.StatusID} GUID={event.Guid}/>
                 <p class="text-gray-400">latitude: {round(event.Latitude)}</p>
@@ -61,4 +75,13 @@
     .unknown {
         border-bottom: 2px solid grey;
     }
+    button {
+	background: none;
+	color: inherit;
+	border: none;
+	padding: 0;
+	font: inherit;
+	cursor: pointer;
+	outline: inherit;
+    }   
 </style>
