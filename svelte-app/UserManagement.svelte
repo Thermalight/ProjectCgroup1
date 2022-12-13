5590c6f02209b955d15c14e5955eda26529addc2
E-Bas <100128509+E-Bas@users.noreply.github.com>
2022-12-13 19:05:52 +0100

    indent

--- svelte-app/UserManagement.svelte
+++ svelte-app/UserManagement.svelte
@@ -47,57 +47,57 @@
     <div style="background-color:#111727;"class="flex justify-center   h-16 rounded-t-2xl p-auto">
         <h1 class="text-2x1 text-white m -auto">Users management</h1>
     </div>
-        <div>
-            <!-- input field to filter users -->
+    <div>
+        <!-- input field to filter users -->
             <div style="background-color:#111727;" class="m-6 grid rounded-2xl content-center">
                 <p class="block mb-2 text-sm font-medium dark:text-white text-white text-center">Filter by name <p class="text-white inline-block">Type in username:</p>
-                <input bind:value={searchTerm} type="Text" />
-            </div>
+        <input bind:value={searchTerm} type="Text" />
+    </div>
         </div>
-        <div>
-            <!-- input form to add users -->
+    <div>
+        <!-- input form to add users -->
             <div style="background-color:#111727;" class="m-6 grid rounded-2xl content-center">
                 <p class="block mb-2 text-sm font-medium dark:text-white text-white text-center">Add User</p>
-                <form on:submit|preventDefault={submitHandler} class="mt-6">
-                    <Input type="text" label="Username" bind:value={UserJson.username} />
-                    <Input type="password" label="Password" bind:value={UserJson.password} />
-                    <Input type="email" label="Email" bind:value={UserJson.email}/>
-                    <label class="text-white" for="isadmin">Is Admin: </label>
-                    <input type="checkbox" label="isAdmin" bind:checked={UserJson.IsAdmin}/><br>
-                    <button class="bg-white px-8 py-2 mt-1 border-none border-r-4 text-gray-800">submit</button>
-                </form>
-            </div>
+        <form on:submit|preventDefault={submitHandler} class="mt-6">
+            <Input type="text" label="Username" bind:value={UserJson.username} />
+            <Input type="password" label="Password" bind:value={UserJson.password} />
+            <Input type="email" label="Email" bind:value={UserJson.email}/>
+            <label class="text-white" for="isadmin">Is Admin: </label>
+            <input type="checkbox" label="isAdmin" bind:checked={UserJson.IsAdmin}/><br>
+            <button class="bg-white px-8 py-2 mt-1 border-none border-r-4 text-gray-800">submit</button>
+        </form>
+    </div>
         </div>
-            
-        {#if updateUserFormOpen}
+
+    {#if updateUserFormOpen}
             <div style="background-color:#111727;" class="m-6 grid rounded-2xl content-center">
                 <p class="block mb-2 text-sm font-medium dark:text-white text-white text-center">Update user </p>
-                <UpdateUserForm user={updateUserJson}/>
+        <UpdateUserForm user={updateUserJson}/>
             </div>
-        {/if}
-            
-        <br><hr>
+    {/if}
+
+    <br><hr>
         <!-- <p class="text-white">Users:</p> -->
         <div style="background-color:#111727;" class="m-6 grid rounded-2xl content-center">
             <p class="block mb-2 text-sm font-medium dark:text-white text-white text-center">Users:</p>
-            <!-- display users -->
-            {#await loadUsers()}
-                <p class="text-white">waiting...</p>
-            {:then data}
-                <div class="users">
-                    {#each data as user}
-                        {#if user.Username.toLowerCase().includes(searchTerm.toLowerCase())}
-                            <div>
-                                <User user={user} /><br>
-                                <button class="text-white" type="submit" on:click={() => deleteUser(user.Guid)}>x</button>
-                                <button class="text-white" type="submit"on:click={() => updateFormHandler(user)}>update</button>
-                            </div>
-                        {/if}
-                    {/each}
-                </div>
-            {:catch error}
-                <p>An error occurred!</p>
-                {console.log(error)}
-            {/await}
+    <!-- display users -->
+    {#await loadUsers()}
+        <p class="text-white">waiting...</p>
+    {:then data}
+        <div class="users">
+            {#each data as user}
+                {#if user.Username.toLowerCase().includes(searchTerm.toLowerCase())}
+                    <div>
+                        <User user={user} /><br>
+                        <button class="text-white" type="submit" on:click={() => deleteUser(user.Guid)}>x</button>
+                        <button class="text-white" type="submit"on:click={() => updateFormHandler(user)}>update</button>
+                    </div>
+                {/if}
+            {/each}
         </div>
+    {:catch error}
+        <p>An error occurred!</p>
+        {console.log(error)}
+    {/await}
+</div>
 </div>
