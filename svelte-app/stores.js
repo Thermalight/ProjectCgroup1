26ce8b7e7b0f15096da1d0b5af9444ed6c06233f
Vincent_de_Gans <vdegans@vsl.nl>
2023-01-11 15:49:36 +0100

    add refreshrate and unread notifs to stores

--- svelte-app/stores.js
+++ svelte-app/stores.js
@@ -3,9 +3,9 @@ import { writable } from "svelte/store";
 export const currentPage = writable("");
 
 export const notifications = writable([]);
+export const refreshRate = writable(1000);
+export const unread = writable(0);
 
 export const probability = writable(-1);
 export const soundType = writable("");
 export const notification_count = writable(10);
-
-export const changedNotifications = writable(false);
