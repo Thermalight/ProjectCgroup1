7a1ad96a0250bcae8e44a0c89d039578e8d808ec
Thermalight <alumite10@gmail.com>
2023-01-11 18:33:33 +0100

    remove notification updater from list

--- svelte-app/List.svelte
+++ svelte-app/List.svelte
@@ -1,46 +1,10 @@
 <script>
     import { slide } from 'svelte/transition';
-    import { onDestroy } from 'svelte';
-    import { probability, soundType, notification_count, notifications, refreshRate, unread } from "./stores.js";
+    import { notifications } from "./stores.js";
     import Notification from "./Notification.svelte"
     import Filter from "./Filter.svelte"
-    import isEqual from 'lodash.isequal';
 
     export let mapComponent
-
-    let clear
-    $: {
-        clearInterval(clear)
-        clear = setInterval(getEvents, $refreshRate)
-    }
-
-    function getEvents() {
-        fetch("https://" + window.location.host + "/notifications?"+ new URLSearchParams({
-            limit: $notification_count,
-            soundType: $soundType,
-            probability: $probability
-        }), {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                "Authorization": 'Bearer ' + localStorage.getItem("token")
-            }
-        })
-        .then(response => {
-            response.json()
-            .then(data => {
-                if (!isEqual($notifications, data)){
-                    $notifications = data;
-                }
-                    
-                $refreshRate = 10000;
-            })
-        })
-    }
-
-    onDestroy(() => {
-        clearInterval(clear);
-    });
 </script>
 <div class="list" transition:slide>
     {#if $notifications }
